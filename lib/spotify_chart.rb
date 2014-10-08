cbc4ef8b1d17873d55280684f89af11d238bc966
thinhthinh <thinhqvu@gmail.com>
2014-10-08 13:45:03 -0400

    Scrape the music from the spotify webz

--- lib/spotify_chart.rb
+++ lib/spotify_chart.rb
@@ -1,19 +1,21 @@
 require 'json'
 require 'open-uri'
+require 'pry'
 
 class SpotifyChart
 
   attr_reader :base_url
 
   def initialize
-    @base_url = "?"
+    @base_url = "http://charts.spotify.com/api/tracks"
   end
 
   def get_url(preference, region)
-    # return a string that is the base url + / + preference + / + region + / + latest
+    "#{@base_url}/#{preference}/#{region}/daily/latest"# return a string that is the base url + / + preference + / + region + / + latest
   end
 
   def get_json(url)
+    JSON.load(open(url))
     # load json given a url here
     # refer to the references if you have questions about this
   end
@@ -36,7 +38,13 @@ class SpotifyChart
     #     }
     #   ]
     # }
+    
+    track_name = music_hash["tracks"][0]["track_name"]
+    artist_name = music_hash["tracks"][0]["artist_name"]
+    album_name = music_hash["tracks"][0]["album_name"]
 
+    "'#{track_name}' by #{artist_name} from the album #{album_name}"
+    
     # given a hash, like the one above, this method should return a string like:
     # '<track name>' by <artist name> from the <album name>
   
@@ -46,7 +54,10 @@ class SpotifyChart
 
 
   def most_streamed(region)
-    # call no get_url here, where preference is the string 'most_streamed',
+    url = get_url("most_streamed",region)
+    hash = get_json(url)
+    fetch_track_album_artist(hash)
+    # call on get_url here, where preference is the string 'most_streamed',
     # and set it equal to a variable
 
     # call on get_json here, using the string that get_url returns
@@ -55,7 +66,10 @@ class SpotifyChart
     # hash that get_json returns
   end
 
-  def most_shared(region)
+  def most_viral(region)
+    url = get_url('most_viral',region)
+    hash = get_json(url)
+    fetch_track_album_artist(hash)
     # call no get_url here, where preference is the string 'most_shared',
     # and set it equal to a variable
 
